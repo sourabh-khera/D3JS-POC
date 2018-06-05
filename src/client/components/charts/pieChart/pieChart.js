38b8ca3883a1a7434fa28c66ea0638a4e5aeb8eb
Sourabh <sourabh.khera@tothenew.com>
2018-06-05 13:13:48 +0530

    pie chart design changed along with seperate loader for every chart and sone css issues fixed

--- src/client/components/charts/pieChart/pieChart.js
+++ src/client/components/charts/pieChart/pieChart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import * as d3 from 'd3';
 import { connect } from 'react-redux';
+import { BounceLoader } from 'react-spinners';
 import PropTypes from 'prop-types';
 import './style.css';
 
@@ -8,9 +9,9 @@ class PieChart extends Component {
   constructor() {
     super();
     this.margin = {
-      top: 20, bottom: 60, left: 40, right: 18,
+      top: 30, bottom: 60, left: 40, right: 60,
     };
-    this.width = 350 - this.margin.left - this.margin.right;
+    this.width = 380 - this.margin.left - this.margin.right;
     this.height = 320 - this.margin.top - this.margin.bottom;
     this.radius = this.width / 2;
   }
@@ -40,10 +41,6 @@ class PieChart extends Component {
       .outerRadius(this.radius - 10)
       .innerRadius(0);
 
-    // const arc = d3.arc()
-    //   .outerRadius(this.radius - 50)
-    //   .innerRadius(this.radius - 50);
-
     const pieTween = b => {
       b.innerRadius = 0;
       const i = d3.interpolate({ startAngle: 0, endAngle: 0 }, b);
@@ -63,17 +60,47 @@ class PieChart extends Component {
       .ease(d3.easeLinear)
       .duration(3000)
       .attrTween('d', pieTween);
-    // g.append('text')
-    //   .attr('transform', d => (`translate(${label.centroid(d)})`))
-    //   .attr('dy', '0.35em')
-    //   .text(d => (d.data.ChannelType));
+
+    const legend = svg.append('g')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', 10)
+      .attr('text-anchor', 'end')
+      .attr('transform', `translate(${-60},${-70})`)
+      .selectAll('g')
+      .data(channelTypeRevenues)
+      .enter()
+      .append('g')
+      .attr('transform', (d, i) => `translate(0, ${i * 20})`);
+
+    legend.append('rect')
+      .attr('x', this.width)
+      .attr('width', 19)
+      .attr('height', 19)
+      .attr('fill', d => color(d.ChannelType));
+
+    legend.append('text')
+      .attr('x', this.width - 4)
+      .attr('y', 9.5)
+      .attr('dy', '0.32em')
+      .text(d => d.ChannelType);
   }
 
   render() {
+    const { showChannelTypeLoader } = this.props;
+    const renderComponent = showChannelTypeLoader ?
+      (
+        <div className="pieLoaderContainer">
+          <BounceLoader
+            color="#123abc"
+            loading
+          />
+        </div>
+      )
+      : <svg ref={node => { this.node = node; }} />;
     return (
       <div className="pieContainer">
         <h3 className="pieHeading">Net Revenue per Channel Type</h3>
-        <svg ref={node => { this.node = node; }} />
+        {renderComponent}
       </div>
     );
   }
@@ -81,11 +108,13 @@ class PieChart extends Component {
 
 const mapStateToProps = state => ({
   channelTypeRevenues: state.revenues.channelTypeRevenues,
+  showChannelTypeLoader: state.revenues.showChannelTypeLoader,
 });
 PieChart.defaultProps = {
   channelTypeRevenues: [],
 };
 PieChart.propTypes = {
   channelTypeRevenues: PropTypes.array,
+  showChannelTypeLoader: PropTypes.bool,
 };
 export default connect(mapStateToProps)(PieChart);
--- src/client/components/charts/verticalBarChart/verticalBarChart.js
+++ src/client/components/charts/verticalBarChart/verticalBarChart.js
@@ -23,12 +23,6 @@ class VerticalBarChart extends Component {
 createChart = () => {
   const { channelTypeRevenues } = this.props;
   const node = this.node;
-  const svg = d3.select(node)
-    .attr('width', this.width + this.margin.right + this.margin.left)
-    .attr('height', this.height + this.margin.top + this.margin.bottom)
-    .append('g')
-    .attr('transform', `translate(${this.margin.left} , ${this.margin.right})`);
-
   const xScale = d3.scaleBand()
     .rangeRound([0, this.width]);
 
@@ -37,33 +31,80 @@ createChart = () => {
 
   const color = d3.scaleOrdinal(['#6b486b', '#8a89a6', '#ff8c00', '#98abc5', '#d0743c']);
 
+
   const xAxis = d3.axisBottom(xScale);
-
   const yAxis = d3.axisLeft(yScale);
-
-  channelTypeRevenues.sort((a, b) => b.GrossRevenue - a.GrossRevenue);
-
-  xScale.domain(channelTypeRevenues.map(d => d.ChannelType));
-  yScale.domain([0, d3.max(channelTypeRevenues, d => d.GrossRevenue)]);
-  const tooltip = d3.select('#div1').append('div')
+  const tooltip = d3.select('body').append('div')
     .style('position', 'absolute')
     .style('opacity', 0)
     .attr('class', 'tooltip')
     .style('display', 'inline-block')
-    .style('padding', '5px')
+    .style('padding', '10px')
     .style('background-color', 'rgba(0,0,0,0.5)')
     .style('color', 'white');
 
-  svg.selectAll('rect')
+  const svg = d3.select(node)
+    .attr('width', this.width + this.margin.left + this.margin.right)
+    .attr('height', this.height + this.margin.top + this.margin.bottom)
+    .append('g')
+    .attr('transform', `translate(${this.margin.left} , ${this.margin.right})`);
+
+
+  xScale.domain(channelTypeRevenues.map(d => d.ChannelType));
+  yScale.domain([0, d3.max(channelTypeRevenues, d => d.GrossRevenue)]);
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', `translate(0, ${this.height})`)
+    .call(xAxis)
+    .selectAll('text')
+    .attr('transform', 'rotate(-60)')
+    .attr('dx', '-.8em')
+    .attr('dy', '.25em')
+    .attr('text-anchor', 'end');
+
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis.ticks(null, 's'))
+    .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('y', 6)
+    .attr('dy', '.71em')
+    .style('text-anchor', 'end')
+    .text('GrossRevenue');
+
+  const legend = svg.append('g')
+    .attr('font-family', 'sans-serif')
+    .attr('font-size', 10)
+    .attr('text-anchor', 'end')
+    .selectAll('g')
     .data(channelTypeRevenues)
     .enter()
-    .append('rect')
+    .append('g')
+    .attr('transform', (d, i) => `translate(0, ${i * 20})`);
+
+  legend.append('rect')
+    .attr('x', this.width)
+    .attr('width', 19)
+    .attr('height', 19)
+    .attr('fill', d => color(d.ChannelType));
+
+  legend.append('text')
+    .attr('x', this.width - 4)
+    .attr('y', 9.5)
+    .attr('dy', '0.32em')
+    .text(d => d.ChannelType);
+  svg.selectAll('.bar')
+    .data(channelTypeRevenues)
+    .enter().append('rect')
+    .attr('class', 'bar')
     .attr('x', d => xScale(d.ChannelType))
     .attr('y', d => yScale(d.GrossRevenue))
     .attr('height', d => this.height - yScale(d.GrossRevenue))
-    .attr('width', 25)
-    .attr('fill', d => color(d.ChannelType))
+    .attr('width', 30)
     .attr('transform', `translate(${22},${0})`)
+    .attr('fill', d => color(d.ChannelType))
+
     .on('mouseover', d => {
       tooltip
         .style('opacity', 1)
@@ -72,25 +113,12 @@ createChart = () => {
     .on('mousemove', () => {
       tooltip
         .style('left', `${d3.event.pageX}px`)
-        .style('top', `${d3.event.pageY - 550}px`);
+        .style('top', `${d3.event.pageY - 40}px`);
     })
     .on('mouseout', () => {
       tooltip
         .style('opacity', 0);
     });
-
-  svg.append('g')
-    .attr('class', 'xAxis')
-    .attr('transform', `translate(0, ${this.height})`)
-    .call(xAxis)
-    .selectAll('text')
-    .attr('transform', 'rotate(-60)')
-    .attr('dx', '-.8em')
-    .attr('dy', '.25em')
-    .attr('text-anchor', 'end');
-  svg.append('g')
-    .attr('class', 'yAxis')
-    .call(yAxis.ticks(null, 's'));
 };
 render() {
   const { showChannelTypeLoader } = this.props;
